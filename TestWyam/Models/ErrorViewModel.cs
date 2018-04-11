1cfe586c1919c1eb85880611208071ed7f5ae52c
Bruno Arruda <bruno.arruda@farfetch.com>
2018-04-11 16:40:50 +0100

    Add Solution

---
+++ TestWyam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestWyam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
